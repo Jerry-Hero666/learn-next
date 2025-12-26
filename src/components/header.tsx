bdb810ae8797bfda4c87cf2e28b4117e5af36f28
Jerry-Hero <1194742322@qq.com>
2025-12-26 15:42:49 +0800

    404页面的建设 | 优化404页面Header不展示

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,21 @@
 "use client";
 import Link from "next/link";
-import { usePathname } from 'next/navigation';
+import { usePathname } from "next/navigation";
 
 const linkData = [
   { href: "/performance", label: "Performance" },
   { href: "/reliability", label: "Reliability" },
   { href: "/scale", label: "Scale" },
-]; 
+];
 
 export default function Header() {
   const pathname = usePathname();
+  const hrefs = [...linkData.map((link) => link.href), "/"];
+
+  if (!pathname || !hrefs.includes(pathname)) {
+    return null;
+  }
+
   return (
     <div className="absolute w-full z-10">
       <div className="flex justify-between container mx-auto p-8 text-white items-center">
@@ -17,17 +23,15 @@ export default function Header() {
           Home
         </Link>
         <div className="text-xl space-x-4">
-          {
-            linkData.map((link) => (
-              <Link
-                key={link.href}
-                href={link.href}
-                className={pathname === link.href ? "text-purple-500" : ""}
-              >
-                {link.label}
-              </Link>
-            ))
-          }
+          {linkData.map((link) => (
+            <Link
+              key={link.href}
+              href={link.href}
+              className={pathname === link.href ? "text-purple-500" : ""}
+            >
+              {link.label}
+            </Link>
+          ))}
         </div>
       </div>
     </div>

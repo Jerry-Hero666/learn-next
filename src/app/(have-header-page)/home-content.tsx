2b52538130da3b87a78d90770dfc842fa39813ff
Jerry-Hero <1194742322@qq.com>
2025-12-29 16:25:56 +0800

    feat: updatehome content

--- src/app/(have-header-page)/home-content.tsx
+++ src/app/(have-header-page)/home-content.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function HomeContent() {
   return (
-    <div className="p-4 bg-white h-full flex flex-col gap-2">
-      <Link className="bg-sky-500 hover:bg-sky-600 rounded-lg p-2 text-white cursor-pointer" href="/blog">点击去博客列表页</Link>
-      <Link className="bg-sky-500 hover:bg-sky-600 rounded-lg p-2 text-white cursor-pointer" href="/parallel">点击去平行路由页面</Link>
+    <div className="p-4 h-full flex flex-col gap-2">
+      <Link className="hover:text-sky-500 rounded-lg p-2 text-white cursor-pointer" href="/blog">点击去博客列表页</Link>
+      <Link className="hover:text-sky-600 rounded-lg p-2 text-white cursor-pointer" href="/parallel">点击去平行路由页面</Link>
     </div>
   );
 }

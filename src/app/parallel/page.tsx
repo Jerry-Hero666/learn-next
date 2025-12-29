cfced8c9107c074f7cf4a976a595a533a9857850
Jerry-Hero <1194742322@qq.com>
2025-12-29 16:19:12 +0800

    添加返回按钮

--- src/app/parallel/page.tsx
+++ src/app/parallel/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 const ParallelPage = () => {
   return (
-    <div className="flex items-center justify-center mt-6 p-6 bg-gray-500 flex-1 text-white rounded-lg">
+    <div>
+      <div className="flex items-center justify-center mt-6 p-6 bg-gray-500 flex-1 text-white rounded-lg">
         children
+      </div>
+      <Link href="/" className="block text-center mt-4 hover:text-blue-500">
+       &rarr; Go to Home
+      </Link>
     </div>
   );
 };
